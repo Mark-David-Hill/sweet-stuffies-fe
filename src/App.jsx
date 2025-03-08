c81b3350491f13eb62a56f5089d3404bf056a3a1
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-03-08 13:16:49 -0700

    adds images and begins to set up styles for first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./styles/main.scss";
 
+import scentedSpecial1 from "./assets/alpaca-front.jpg";
+import scentedSpecial2 from "./assets/capybara-front.jpg";
 import logo from "./assets/logo.svg";
 
 function App() {
@@ -20,10 +22,17 @@ function App() {
         </div>
       </div>
       <div className="home-container">
-        <div className="ad">Ad</div>
+        <div className="section-1">
+          <div class="image-banner">
+            <div class="overlay-text">SCENTED SPECIALS</div>
+          </div>
+          <img className="scented-special-2" src={scentedSpecial2} alt="" />
+          <button>Buy Now</button>
+        </div>
+        {/* <div className="ad">Ad</div>
         <div className="section">Content</div>
         <div className="ad">Ad</div>
-        <div className="section">Content</div>
+        <div className="section">Content</div> */}
       </div>
     </div>
   );
