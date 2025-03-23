bb80832db5030a0f9d81b82a77d3c70965508dbb
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-03-23 17:53:42 -0600

    adds font, sets up styling for contact button, and makes other landing page adjustments

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -2,17 +2,6 @@ import Navbar from "../navigation/Navbar";
 
 import Slideshow from "../features/Slideshow";
 
-// import chocolateChipCookies from "../../assets/chocolate-chip-cookies.jpg";
-// import stuffiesCollection from "../../assets/stuffies-collection.jpg";
-// import blueberryMuffins from "../../assets/blueberry-muffins.jpg";
-// import scentedSpecial2 from "../../assets/capybara-front.jpg";
-// import appleCinnamon from "../../assets/apple-cinnamon.jpg";
-// import birthdayCake from "../../assets/birthday-cake.jpg";
-// import blueYoshi from "../../assets/blue-yoshi-1.jpg";
-// import teddyBear from "../../assets/teddy-bear-1.jpg";
-// import unicorn from "../../assets/unicorn.jpg";
-// import koroks from "../../assets/koroks.jpg";
-
 import deliveryImg from "../../assets/landing-page/delivery.jpg";
 import s1i1 from "../../assets/landing-page/slideshow-1/s1i1.jpg";
 import s1i2 from "../../assets/landing-page/slideshow-1/s1i2.jpg";
@@ -41,49 +30,60 @@ function LandingPage() {
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="slideshow-1-wrapper">
-        {/* <img src={s1i1} alt="" />
+        <div className="slideshow-1-wrapper">
+          {/* <img src={s1i1} alt="" />
           <img src={s1i5} alt="" />
           <img src={s1i6} alt="" /> */}
-        <Slideshow images={[s1i2, s1i3, s1i4, s1i7, s1i8, s1i9, s1i10]} />
-      </div>
-      <main>
+          <Slideshow images={[s1i2, s1i3, s1i4, s1i7, s1i8, s1i9, s1i10]} />
+        </div>
         <div className="contact-button-wrapper">
           <button>Book Your Free Consultation With Me Today!</button>
         </div>
+      </div>
+
+      <main>
         <div className="process-section">
           <h2>How It Works</h2>
-          <h3>Step 1: Consultation</h3>
-          <p>See what I can do, tell me what you want</p>
-          {/* <Slideshow /> */}
-          <button>Book Your Free Consultation With Me Today!</button>
-          <h3>Step 2: Creation</h3>
-          <p>I work my magic</p>
-          {/* <Slideshow /> */}
-          <button>Book Your Free Consultation With Me Today!</button>
-          <h3>Step 3: Elation</h3>
-          <p>Cute cuddly-ness delivered straight to you</p>
+          <div className="step-wrapper">
+            <h3>Step 1: Consultation</h3>
+            <p>See what I can do, tell me what you want</p>
+          </div>
+          <div className="slideshow-1-wrapper">
+            <Slideshow images={[s1i2, s1i3, s1i4, s1i7, s1i8, s1i9, s1i10]} />
+          </div>
+          <div className="contact-button-wrapper">
+            <button>Book Your Free Consultation With Me Today!</button>
+          </div>
+          <div className="step-wrapper">
+            <h3>Step 2: Creation</h3>
+            <p>I work my magic</p>
+          </div>
+          <div className="slideshow-1-wrapper">
+            <Slideshow images={[s1i2, s1i3, s1i4, s1i7, s1i8, s1i9, s1i10]} />
+          </div>
+          <div className="contact-button-wrapper">
+            <button>Book Your Free Consultation With Me Today!</button>
+          </div>
+          <div className="step-wrapper">
+            <h3>Step 3: Elation</h3>
+            <p>Cute cuddly-ness delivered straight to you</p>
+          </div>
           <img
             className="delivery-img"
             src={deliveryImg}
             alt="Delivery Stock photos by Vecteezy"
           />
-          <button>Book Your Free Design Consult With Me Today!</button>
+          <div className="contact-button-wrapper">
+            <button>Book Your Free Design Consult With Me Today!</button>
+          </div>
+        </div>
+        <div className="calendly-wrapper">
+          <h2>Calendly Section</h2>
         </div>
         <div className="end-section">
           <h2>Questions? Concerns? Contact me at:</h2>
           <a href="sweet.stuffies.em@gmail.com">sweet.stuffies.em@gmail.com</a>
         </div>
-
-        {/* <div className="slideshow-container">
-        <div className="slides">
-          <img src={teddyBear} alt="" />
-          <img src={unicorn} alt="" />
-          <img src={koroks} alt="" />
-        </div>
-      </div> */}
       </main>
     </div>
   );
