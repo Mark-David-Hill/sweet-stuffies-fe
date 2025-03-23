3312e2f54149231b24b81b92ec829eb9f532cfc4
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-03-23 16:51:06 -0600

    sets up overall styling and spacing for main section of the landing page

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -42,42 +42,49 @@ function LandingPage() {
           </div>
         </div>
       </div>
+
       <div className="slideshow-1-wrapper">
         {/* <img src={s1i1} alt="" />
           <img src={s1i5} alt="" />
           <img src={s1i6} alt="" /> */}
         <Slideshow images={[s1i2, s1i3, s1i4, s1i7, s1i8, s1i9, s1i10]} />
       </div>
-      <div className="contact-button-wrapper">
-        <button>Book Your Free Consultation With Me Today!</button>
-      </div>
-      <h2>How It Works</h2>
-      <div className="process-section">
-        <h3>Step 1: Consultation</h3>
-        <p>See what I can do, tell me what you want</p>
-        {/* <Slideshow /> */}
-        <button>Book Your Free Consultation With Me Today!</button>
-        <h3>Step 2: Creation</h3>
-        <p>I work my magic</p>
-        {/* <Slideshow /> */}
-        <button>Book Your Free Consultation With Me Today!</button>
-        <h3>Step 3: Elation</h3>
-        <p>Cute cuddly-ness delivered straight to you</p>
-        <img src={deliveryImg} alt="Delivery Stock photos by Vecteezy" />
-        <button>Book Your Free Design Consult With Me Today!</button>
-      </div>
-      <div className="end-section">
-        <h2>Questions? Concerns? Contact me at:</h2>
-        <a href="sweet.stuffies.em@gmail.com">sweet.stuffies.em@gmail.com</a>
-      </div>
+      <main>
+        <div className="contact-button-wrapper">
+          <button>Book Your Free Consultation With Me Today!</button>
+        </div>
+        <div className="process-section">
+          <h2>How It Works</h2>
+          <h3>Step 1: Consultation</h3>
+          <p>See what I can do, tell me what you want</p>
+          {/* <Slideshow /> */}
+          <button>Book Your Free Consultation With Me Today!</button>
+          <h3>Step 2: Creation</h3>
+          <p>I work my magic</p>
+          {/* <Slideshow /> */}
+          <button>Book Your Free Consultation With Me Today!</button>
+          <h3>Step 3: Elation</h3>
+          <p>Cute cuddly-ness delivered straight to you</p>
+          <img
+            className="delivery-img"
+            src={deliveryImg}
+            alt="Delivery Stock photos by Vecteezy"
+          />
+          <button>Book Your Free Design Consult With Me Today!</button>
+        </div>
+        <div className="end-section">
+          <h2>Questions? Concerns? Contact me at:</h2>
+          <a href="sweet.stuffies.em@gmail.com">sweet.stuffies.em@gmail.com</a>
+        </div>
 
-      {/* <div className="slideshow-container">
+        {/* <div className="slideshow-container">
         <div className="slides">
           <img src={teddyBear} alt="" />
           <img src={unicorn} alt="" />
           <img src={koroks} alt="" />
         </div>
       </div> */}
+      </main>
     </div>
   );
 }
