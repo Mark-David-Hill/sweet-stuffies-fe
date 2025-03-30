68a612e4892f9b7743ac4797e441218c6469a2c5
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2025-03-30 15:11:12 -0600

    refactors slideshow component and removes commented code

--- src/components/features/Slideshow.jsx
+++ src/components/features/Slideshow.jsx
@@ -1,38 +1,24 @@
 import { useState, useEffect } from "react";
-// import chocolateChipCookies from "../../assets/chocolate-chip-cookies.jpg";
-// import stuffiesCollection from "../../assets/stuffies-collection.jpg";
-// import blueberryMuffins from "../../assets/blueberry-muffins.jpg";
-
-// const images = [
-//   chocolateChipCookies,
-//   stuffiesCollection,
-//   blueberryMuffins,
-//   chocolateChipCookies,
-//   stuffiesCollection,
-//   blueberryMuffins,
-// ];
 
 function Slideshow({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
 
-  // Auto-slide effect
   useEffect(() => {
     if (isPaused) return;
 
     const interval = setInterval(() => {
       nextSlide();
-    }, 5000); // Change slide every 5 seconds
+    }, 5000);
+    seconds;
 
     return () => clearInterval(interval);
   }, [currentIndex, isPaused]);
 
-  // Move to next slide
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  // Move to previous slide
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
@@ -54,10 +40,10 @@ function Slideshow({ images }) {
         ))}
       </div>
 
-      {/* Navigation Arrows */}
       <button className="prev" onClick={prevSlide}>
         &#10094;
       </button>
+
       <button className="next" onClick={nextSlide}>
         &#10095;
       </button>
